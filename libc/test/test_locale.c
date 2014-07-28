589be85d05164f75decbd65fdbc2506b8850d7a4
Li Yun <leven.cn@gmail.com>
2014-07-28 10:44:09 +0800

    [libc] Add test cases for <locale.h>

---
+++ libc/test/test_locale.c
@@ -0,0 +1,41 @@
+/**
+ * Test <locale.h>
+ *
+ *
+ * Copyright 2014 Li Yun <leven.cn@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <locale.h>
+
+int main()
+{
+    char *curr_locale = setlocale(LC_ALL, NULL); // get current locale
+    
+    /* set locale.
+    
+    On startup of the main program, the portable "C" locale is selected as
+    default.
+    
+    The command `locale` could check current system locale and `locale -a`
+    could query all available locales.
+    
+    For most cases, there is no need to check the return value of
+    `setlocale()`.
+    */
+    setlocale(LC_ALL, "");  // set to system locale
+    setlocale(LC_ALL, "C"); // "C" == "POSIX"
+    
+    return 0;
+}

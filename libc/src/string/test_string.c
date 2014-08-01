83b88589c7b56bc0f16846c702ef20b21edf9e58
Li Yun <leven.cn@gmail.com>
2014-08-01 18:52:39 +0800

    [libc:string] Pass testing on Mac OS X 10.9.4
    - An issue for strncpy() on Mac OS X 10.9.4

--- libc/src/string/test_string.c
+++ libc/src/string/test_string.c
@@ -20,6 +20,7 @@
 #include "func_string.h"
 #include <string.h>
 #include <assert.h>
+//#include <stdio.h> an issue on Mac OS X 10.9.4
 
 
 static char *src = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
@@ -29,7 +30,7 @@ int main()
     /* Test strlen() */
     assert(func_strlen("") == strlen(""));
     assert(func_strlen("string") == strlen("string"));
-    
+
     /* Test strcmp() */
     assert(func_strcmp("abc", "bbc") < 0);
     assert(func_strcmp("bbc", "abc") > 0);
@@ -38,7 +39,7 @@ int main()
     assert(func_strcmp("aac", "abc") < 0);
     assert(func_strcmp("abcde", "abc") > 0);
     assert(func_strcmp("aacde", "abc") < 0);
-    
+
     /* Test strncmp() */
     assert(func_strncmp("abc","bbc", 3) < 0);
     assert(func_strncmp("bbc", "abc", 3) > 0);
@@ -48,7 +49,7 @@ int main()
     assert(func_strncmp("abcde", "abc", 5) > 0);
     assert(func_strncmp("aacde", "abc", 5) < 0);
     assert(func_strncmp("abcde", "abc", 3) == 0);
-    
+
     /* Test memcmp() */
     assert(func_memcmp("abc","bbc", 3) < 0);
     assert(func_memcmp("bbc", "abc", 3) > 0);
@@ -58,40 +59,41 @@ int main()
     assert(func_memcmp("abcde", "abc", 5) > 0);
     assert(func_memcmp("aacde", "abc", 5) < 0);
     assert(func_memcmp("abcde", "abc", 3) == 0);
-    
+
     /* Test strncat() */
     char dst_stdc[8] = "a";
     char dst_func[8] = "a";
     size_t n = 8 - strlen(dst_stdc) - 1;
     assert(strcmp(strncat(dst_stdc,src,n), func_strncat(dst_func,src,n)) == 0);
-    
+
     /* Test strncpy() */
     char dst_strncpy_stdc[8];
     char dst_strncpy_func[8];
     strncpy(dst_strncpy_stdc, src, 7);
     func_strncpy(dst_strncpy_func, src, 7);
-    assert(strcmp(dst_strncpy_stdc, dst_strncpy_stdc)==0 \
+    //puts(dst_strncpy_stdc); // On Mac OS X 10.9.4, passed by adding this line
+    assert(strcmp(dst_strncpy_stdc, dst_strncpy_func)==0 \
             && strlen(dst_strncpy_stdc)==strlen(dst_strncpy_func));
-            
+
     /* Test memcpy() */
     char dst_memcpy_stdc[8];
     char dst_memcpy_func[8];
     memcpy(dst_memcpy_stdc, src, 7);
     func_memcpy(dst_memcpy_func, src, 7);
     assert(memcmp(dst_memcpy_stdc, dst_memcpy_func, 7) == 0);
-            
+
     /* Test memmove() */
     char dst_memmove_stdc[8];
     char dst_memmove_func[8];
     memmove(dst_memmove_stdc, src, 7);
     func_memmove(dst_memmove_func, src, 7);
     assert(memcmp(dst_memmove_stdc, dst_memmove_func, 7) == 0);
-            
+
     /* Test memset() */
     char memset_buf[6] = "123456";
     assert(memcmp(memset(memset_buf, 0, 6), "\0\0\0\0\0\0", 6) == 0);
     assert(func_memcmp(memset(memset_buf, 0, 6), "\0\0\0\0\0\0", 6) == 0);
-    
+
     /* Test strchr(), strrchr() and strstr() */
     char *s_strchr = "123456";
     for (char *s=s_strchr; *s!='\0'; s++) {
@@ -103,6 +105,6 @@ int main()
                 == func_memchr(s_strchr, (int)(*s), strlen(s_strchr)));
         assert(strstr(s_strchr, s) == func_strstr(s_strchr, s));
     }
-    
+
     return 0;
 }
